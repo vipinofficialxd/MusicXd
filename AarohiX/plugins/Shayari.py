808db9e94657db806e103f74b0e13aaa0abb790d
Radha xD <133753917+Radhak8@users.noreply.github.com>
2023-07-06 07:57:00 +0530

    Update Shayari.py

--- AarohiX/plugins/Shayari.py
+++ AarohiX/plugins/Shayari.py
@@ -66,9 +66,9 @@ async def help(client: Client, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        "✨sᴜᴘᴘᴏʀᴛ✨", url=f"https://t.me/LOVE_FEELINGS_WILL1"),
+                        "✨sᴜᴘᴘᴏʀᴛ✨", url=f"https://t.me/RadhaX2Support"),
                     InlineKeyboardButton(
-                        "✨ᴏᴡɴᴇʀ✨", url=f"https://t.me/HONEY_SINGH_121")
+                        "✨ᴏᴡɴᴇʀ✨", url=f"https://t.me/GHOSTRADHA")
                     
                 ]
             ]
